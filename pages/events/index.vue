b261aefd490f4fe35206323a3d1e19ff531d546a
Dixy-05 <66582032+Dixy-05@users.noreply.github.com>
2021-12-30 15:11:44 -0600

    buefy pagination

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,15 +1,29 @@
 <template lang="pug">
-div
-	h1 Events
-	EventCard(v-for="(event,index) in events" :key="index" :event="event" :data-index="index")
-	div.pagination   
-		nuxt-link(:to="{path:'/events',query:{page:page+1}}" ) next Pg &#10097;
-		nuxt-link(:to="{path:'/events',query:{page:page-1}}" v-if="page !=1") &#10096; prev Pg
+  div
+    h1 Events
+    EventCard(v-for="(event,index) in events" :key="index" :event="event" )
+    section
+      b-pagination(:total="totalEvents" v-model='current' :per-page="eventsPerPage"  order="is-right")
+        template(#default='props')
+          b-pagination-button(:page='props.page' :id='`page${props.page.number}`' tag='nuxt-link' :to='`/events?page=${props.page.number}`')
+            | {{(props.page.number) }}
+
+        template(#previous='props')
+          b-pagination-button(:page='props.page' tag='nuxt-link' :to='`/events?page=${!props.page.disabled?props.page.number:$route.query.page}`' )
+            b-icon(icon="chevron-left")
+            span Prev
+
+        template(#next='props')
+          b-pagination-button(:page='props.page' tag='nuxt-link' :to='`/events?page=${!props.page.disabled?props.page.number:$route.query.page}`' )
+            span Next
+            b-icon(icon="chevron-right")
 </template>
 
 <script>
 // section(class="section")
 //   div(class="columns is-mobile")
+// nuxt-link(:to="{path:'/events',query:{page:page+1}}" ) next Pg &#10097;
+// nuxt-link(:to="{path:'/events',query:{page:page-1}}" v-if="page !=1") &#10096; prev Pg
 
 import { mapState } from 'vuex'
 import EventCard from '~/components/eventCard'
@@ -22,7 +36,9 @@ export default {
   // asyncData  is called each time before the page component is loaded
   data() {
     return {
-      page: +this.$route.query.page || 1,
+      current: +this.$route.query.page || 1,
+      rangeBefore: 2,
+      rangeAfter: 1,
     }
   },
   async fetch({ store, error, route }) {
@@ -43,7 +59,11 @@ export default {
       title: 'Event Listing',
     }
   },
-  computed: mapState({ events: (state) => state.events.events }),
+  computed: mapState({
+    events: (state) => state.events.events,
+    totalEvents: (state) => state.events.totalEvents,
+    eventsPerPage: (state) => state.events.eventsPerPage,
+  }),
   watchQuery: ['page'],
 }
 </script>
--- store/events.js
+++ store/events.js
@@ -1,6 +1,8 @@
 import EventService from '~/services/EventService.js'
 export const state = () => ({
   events: [],
+  totalEvents: null,
+  eventsPerPage: null,
   event: {},
   eventNav: {
     details: false,
@@ -14,7 +16,9 @@ export const state = () => ({
 })
 export const mutations = {
   SET_EVENTS(state, events) {
-    state.events = events
+    state.events = events.data
+    state.totalEvents = events.total
+    state.eventsPerPage = events.perPage
   },
   SET_EVENT(state, event) {
     state.event = event
@@ -35,9 +39,12 @@ export const mutations = {
 export const actions = {
   async fetchEvents({ commit }, payload) {
     const response = await EventService.getEvents(payload.perPage, payload.page)
-    // console.log(payload)
-    // console.log(response.headers['x-total-count'])
-    return commit('SET_EVENTS', response.data)
+    const totalCount = response.headers['x-total-count']
+    return commit('SET_EVENTS', {
+      data: response.data,
+      total: totalCount,
+      perPage: payload.perPage,
+    })
   },
   async fetchEvent({ commit }, id) {
     const response = await EventService.getEvent(id)
